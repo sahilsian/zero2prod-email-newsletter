e40c0f08f2b26ca4c438119bb6901af126e15014
Sahil Sian <sahilsiantech@gmail.com>
2026-06-14 12:17:45 -0700

    updated docker file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,14 @@
-use config;
 use secrecy::{ExposeSecret, Secret};
+
+pub enum Environment {
+    Local,
+    Production
+}
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application: ApplicationSettings
 }
 
 #[derive(serde::Deserialize, Clone)]
@@ -14,6 +18,12 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String
 }
 
 impl DatabaseSettings {
@@ -26,11 +36,54 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+
+    // get base bath from current environment
+    let base_path = std::env::current_dir()
+        .expect("Failed to determine the current directory");
+    // join config dir
+    let configuration_directory = base_path.join("configuration");
+
+    // detect curr env
+    let environment:Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-    .add_source(
-        config::File::new("configuration.yaml", config::FileFormat::Yaml)
-    )
-    .build()?;
+        .add_source(
+            config::File::from(configuration_directory.join("base.yaml"))
+        )
+        .add_source(
+            config::File::from(configuration_directory.join(environment_filename))
+        )
+        .build()?;
 
     settings.try_deserialize::<Settings>()
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production"
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment \
+                Use either `local` or `production`.",
+                other
+            ))
+        }
+    }
 }
